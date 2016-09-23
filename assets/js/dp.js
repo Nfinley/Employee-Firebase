61e274c103326b0e929c712956190d462855f791
Matt Holland <mtholland10@gmail.com>
2016-09-23 14:29:01 -0500

    Update dp functionality

---
+++ assets/js/dp.js
@@ -0,0 +1,19 @@
+// Establish reliable container
+var html = '<div class="drunk-ass-div"></div>';
+
+// Initialize global array var, to allow for major program scope penetration
+var masterPackageArray = ["dp1", "dp2", "dp3", "dp4"];
+
+// Loop through items inside of masterPackageArray, assigning part of it to private var each time, for maintaining proper fidelity
+var stuff = "";
+masterPackageArray.forEach(function(dickPick) {
+
+	// Establish ownership
+	var myPeePee = dickPick;
+
+	// Build html string to throw into its proper package
+	stuff += '<img src="' + myPeePee + '">';
+});
+
+// Put packages into their reliable container, and reveal to user
+$('.drunk-ass-div').html(stuff);
